0ccca6fb9c176305089048fd69e5ae9930647ead
umangsingh-05 <umang.25b01010438@abes.ac.in>
2026-04-07 17:02:11 +0530

    done

--- EXPERIMENT 4/expe4.js
+++ EXPERIMENT 4/expe4.js
@@ -1,45 +1,37 @@
-function calculateResult()
-{
-    let n = document.getElementById("subjects").value;
-    let totalMarks = 0;
-
-    for(let i=1; i<=n; i++)
-    {
-        let marks = parseFloat(prompt("Enter marks for subject " + i));
-        totalMarks += marks;
-    }
-    let average = totalMarks / n;
-    let result ;
-    let grade;
-
-    if(average >= 90)
-    {
-        grade = "A";
-    }
-    else if(average >= 80)
-    {
-        grade = "B";
-    }
-    else if(average >= 70)
-    {
-        grade = "C";
-    }
-    else if(average >= 60)
-    {
-        grade = "D";
-    }
-    else
-    {
-        grade = "F";
-    }
-    if(average >= 60)
-    {
-        result = "Pass";
-    }
-    else
-    {
-        result = "Fail";
-    }
-    document.getElementById("result").innerHTML = 
-    "Total Marks: " + totalMarks + "<br>" + "Average Marks: " + average + "<br>" +"Grade: " + grade + "<br>" +"Result: " + result;
+function calculateResult(){ 
+let n = document.getElementById("subjects").value; 
+let total = 0; 
+for(let i=1;i<=n;i++){ 
+let marks = parseFloat(prompt("Enter marks for Subject " + i)); 
+total = total + marks; 
+} 
+let average = total / n; 
+let grade; 
+let result; 
+if(average >= 90){ 
+grade = "A+"; 
+} 
+else if(average >= 75){ 
+grade = "A"; 
+} 
+else if(average >= 60){ 
+grade = "B"; 
+} 
+else if(average >= 50){ 
+grade = "C"; 
+} 
+else{ 
+grade = "F"; 
+} 
+if(average >= 40){ 
+result = "PASS"; 
+} 
+else{ 
+result = "FAIL"; 
+} 
+document.getElementById("result").innerHTML = 
+"Total Marks: " + total + "<br>" + 
+"Average Marks: " + average.toFixed(2) + "<br>" + 
+"Grade: " + grade + "<br>" + 
+"Result: " + result; 
 }
--- xyz.js
+++ xyz.js
@@ -14,11 +14,66 @@
 // const name = "umang";
 // console.log(name);
 
-let a="umang";
-console.log(typeof a);
-let b=10;
-console.log(typeof b);
-let c=true;
-console.log(typeof c);
-let d;
-console.log(typeof d);
+// let a="umang";
+// console.log(typeof a);
+// let b=10;
+// console.log(typeof b);
+// let c=true;
+// console.log(typeof c);
+// let d;
+// // console.log(typeof d);
+
+
+// const add = (a,b) => a+b;
+// console.log(add(5,3));           arrow function
+
+// let numbers =[1, 2, 3, 4];
+// let square=numbers.map(num => num*num);
+// console.log(square);
+
+
+
+
+// let numbers =[10, 15 ,20, 25];
+// let result = numbers.filter(num => num>15);            
+// console.log(result);                             filter()
+
+// let students ={
+//     name: "umang",
+//     age: 20,
+// };
+// let jsonData = JSON.stringify(students);             javaScript object to JSON string
+// console.log(jsonData);
+
+
+
+// let jsonData = '{"name": "umang", "age": 20}';
+// let students = JSON.parse(jsonData);
+// console.log(students);
+
+// let numbers = [1, 2, 3, 4, 5];
+// for(let value of numbers){
+//     console.log(value);
+// }
+
+
+// const add = (a,b) => a+b;
+// console.log(add(5,3));           arrow function
+
+
+let promise = new Promise((resolve, reject) => {
+    let success = true;
+    if(success){
+        resolve("Promise resolved successfully!");
+    } else {
+        reject("Promise rejected.");
+    }
+});
+
+promise.then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
+});
+
+
